6c9fcef566291686bcb16937b889a30485096bb4
sylvainBenoist <benoistsylvain33@gmail.com>
2020-03-20 16:31:46 +0100

    changement index
    création des header footer

--- footer.php
+++ footer.php
@@ -1,9 +1,2 @@
-<?php
-
-echo '';
-
-?>
-
-<div style="background-color:#00868b;width:100%;height: 400px;position: fixed;top: 90%;left: 0;padding-top: 25px;">
-<div style="margin:auto;text-align: center;font-family: arial;color:#faebd7;font-size: 55px;">FOOTER</div>
-</div>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,9 +1,28 @@
 <?php
-
-echo '';
-
+session_start();
 ?>
-
-<div style="background-color:#fd9c93;width:100%;height: 100px;position: fixed;top: 0;left: 0;padding-top: 40px;">
-<div style="margin:auto;text-align: center;font-family: arial;color:#faebd7;font-size: 55px;">HEADER</div>
-</div>
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="UTF-8">
+    <title>Groupe B2</title>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="form_contact.php">Contact</a></li>
+            <li><a href="#">Shop</a></li>
+            <?php
+            if(isset($_SESSION['id'])){
+                echo '<li><a href="#">Logout</a></li>';
+            }else{
+                echo '<li><a href="#">Login</a></li>';
+            }
+            ?>
+            <ul>
+                <nav>
+                    <header>

03df451a3398c704c416d133b000920ad88eb648
hellpapa <frederic.pereira@epsi.fr>
2020-03-20 17:55:15 +0100

    Update connexion.php
    modification de la redirection

--- assets/php/connexion.php
+++ assets/php/connexion.php
@@ -5,15 +5,10 @@ if(isset($_POST['form_login'])) {
     $email = htmlspecialchars($_POST['Email']);
     $prenom = $_POST['prenom'];
     $nom = $_POST['nom'];
-    $sql = "INSERT INTO users (prenom, nom, email) VALUES ('$prenom', '$nom', '$email')";
-
-    if ($bdd->query($sql) == TRUE) {
-        echo "Message enregistré";
-
-    } else {
-        echo "Erreur: " . $sql . "<br>" . $bdd->error;
-    }
-    header("location : ../../index.html");
+    $sql = "INSERT INTO users (prenom, nom, email) VALUES (:prenom, :nom, :email)";
+    $sth=$bdd->prepare($sql, array(PDO::ATTR_CURSOR=>PDO::CURSOR_FWDONLY));
+    $sth->execute(array(':prenom'=>$prenom, ':nom'=>$nom,':email'=>$email));
+    header("Location: ../../index.html");
     exit();
 }
 ?>
